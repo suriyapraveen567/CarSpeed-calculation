a093716a9e494914a1337b56c9cb2e1a261c4a7a
Suriya Praveen <109650417+suriyapraveen567@users.noreply.github.com>
2022-08-29 02:03:06 -0700

    Update Program.cs

--- CarSpeed calculation/Program.cs
+++ CarSpeed calculation/Program.cs
@@ -1,73 +1,49 @@
-// Online C# Editor for free
-// Write, Edit and Run your C# code using C# Online Compiler
-
 using System;
 using System.Collections;
 using System.Collections.Generic;
-
 public class Dataset
 {
-	public List<double> distance = new List<double>();
-	public List<double> time = new List<double>();
-	public List<double> speed = new List<double>();
-	public int value;
+    public List<double>distance = new List<double>();
+    public List<double> time = new List<double>(); 
 }
-
 public class UserInput
 {
-	public void run()
-	{
-		Dataset obj = new Dataset();
-		Console.WriteLine("Enter Number of Inputs");
-		string input = Console.ReadLine();
-		int count = Convert.ToInt32(input);
-		obj.value = count;
-		obj.distance = new List<double>(count);
-		for (int i = 0; i < count; i++)
-		{
-			Console.WriteLine("Enter the distance");
-			string distance = Console.ReadLine();
-			obj.distance.Add(Convert.ToDouble(distance));
-			Console.WriteLine("Enter the time");
-			string time = Console.ReadLine();
-			obj.time.Add(Convert.ToDouble(time));
-		}
-		Measure measure = new Measure();
-		for (int i = 0; i < count; i++)
-		{
-			double temp = measure.run2(obj.distance[i], obj.time[i]);
-			obj.speed.Add(temp);
-		}
-	}
+    public void run()
+    {
+        Dataset obj = new Dataset();
+        Console.WriteLine(" Enter the number of userinputs ");
+        string inputs = Console.ReadLine();
+        int count = Convert.ToInt32(inputs);
+        obj .distance = new List<double>();
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine(" Enter the distance ");
+            string distance = Console.ReadLine();
+            obj .distance.Add(Convert.ToDouble(distance));
+            Console.WriteLine(" Enter the time ");
+            string time = Console.ReadLine();
+            obj.time.Add(Convert.ToDouble(time));
+        }
+        Measure measure = new Measure();
+        for( int i = 0; i < count; i++)
+        {
+            measure.run2(obj.distance[i], obj.time[i], i);
+        }
+    }
 }
-
 public class Measure
 {
-	public double run2(double a, double b)
-	{
-		return a / b;
-	}
+    public void run2( double a,double b,int i)
+    {
+        Console.WriteLine(" output of user " + (i+1));
+        Console.WriteLine(a / b);
+    }
 }
-
-public class show_result
+public class MainMethod
 {
-	public void run3()
-	{
-		Dataset obj = new Dataset();
-		for (int i = 0; i < obj.value; i++)
-		{
-			Console.WriteLine(obj.speed[i]);
-		}
-	}
-}
-
-public class first
-{
-	public static void Main(string[] args)
-	{
-		UserInput c = new UserInput();
-		show_result obj = new show_result();
-		c.run();
-		obj.run3();
-	}
+    public static void Main(string[]args)
+    {
+        UserInput c = new UserInput();
+        c.run();
+    }
 }
